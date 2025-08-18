d39913ede8fc91adf7912a117c54f8043b252e98
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-08-18 12:38:44 +0200

    init finish, next step is parsing

--- Include/cub3D.h
+++ Include/cub3D.h
@@ -3,13 +3,21 @@
 //struc from struct.h
 # include "struct.h"
 //define for all the define parts
-#include "define.h"
+# include "define.h"
+//minilbx
 # include "MLX42/include/MLX42/MLX42.h"
+//libft
 # include "libft/libft.h"
+//Math 
 # include "math.h"
 //initialisation
 void	init_player(t_player *player, double start_x, double start_y, char start);
 void	init_game(t_game *game);
 void	init_textures(t_texture *textures);
+void	init_images(t_game *game);
+void	init_window(t_game *game);
+//tools
+void	*check_malloc(size_t size);
+void	error_function(const char *message);
 
 # endif
