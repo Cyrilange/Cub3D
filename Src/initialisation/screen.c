34da6376da4739fee8d30b3bbb9c59d5fd27c9a5
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-08-18 13:07:38 +0200

    init finish, next step is parsing

--- Src/initialisation/screen.c
+++ Src/initialisation/screen.c
@@ -9,14 +9,58 @@ void	init_window(t_game *game)
 	}
 }
 
+static void	image_north(t_game *game)
+{
+    game->texture.no = mlx_load_png(game->texture.no_path);
+    if (!game->texture.no)
+        error_function("Error : north texture issue");
+    mlx_image_t *no_img = mlx_texture_to_image(game->mlx, game->texture.no);
+    if (!no_img)
+        error_function("Error : north image issue");
+    mlx_image_to_window(game->mlx, no_img, 0, 0);
+}
+
+static void	image_south(t_game *game)
+{
+    game->texture.so = mlx_load_png(game->texture.so_path);
+    if (!game->texture.so)
+        error_function("Error : south texture issue");
+    mlx_image_t *so_img = mlx_texture_to_image(game->mlx, game->texture.so);
+    if (!so_img)
+        error_function("Error : south image issue");
+    mlx_image_to_window(game->mlx, so_img, 100, 0);
+}
+
+static void	image_east(t_game *game)
+{
+    game->texture.ea = mlx_load_png(game->texture.ea_path);
+    if (!game->texture.ea)
+        error_function("Error : east texture issue");
+    mlx_image_t *ea_img = mlx_texture_to_image(game->mlx, game->texture.ea);
+    if (!ea_img)
+        error_function("Error : east image issue");
+    mlx_image_to_window(game->mlx, ea_img, 200, 0);
+}
+
+static void	image_west(t_game *game)
+{
+    game->texture.we = mlx_load_png(game->texture.we_path);
+    if (!game->texture.we)
+        error_function("Error : west texture issue");
+    mlx_image_t *we_img = mlx_texture_to_image(game->mlx, game->texture.we);
+    if (!we_img)
+        error_function("Error : west image issue");
+    mlx_image_to_window(game->mlx, we_img, 300, 0);
+}
+
 void	init_images(t_game *game)
 {
-	// Exemple pour charger une texture Nord :
-	game->texture.no = mlx_load_png(game->texture.no_path);
-	if (!game->texture.no)
-		exit(1);
-	mlx_image_t *north_img = mlx_texture_to_image(game->mlx, game->texture.no);
-	if (!north_img)
-		exit(1);
-	mlx_image_to_window(game->mlx, north_img, 0, 0);
+	if (game->texture.no_path)
+		image_north(game);
+	if (game->texture.so_path)
+		image_south(game);
+    if (game->texture.ea_path)
+		image_east(game);
+    if (game->texture.we_path)
+		image_west(game);
 }
