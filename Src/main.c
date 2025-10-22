c64bd71b08859d0be496497740159f8acc641396
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-10-22 16:52:34 +0200

    d

--- Src/main.c
+++ Src/main.c
@@ -1,21 +1,5 @@
 #include "cub3D.h"
 
-static char *call_cub(char  *map_path)
-{
-    // Définir le préfixe du chemin
-    const char *prefix = "Include/";
-    char *full_path;
-
-    full_path = malloc(strlen(prefix) + ft_strlen(map_path) + 1);
-    if (!full_path)
-    {
-        printf("Erreur: Échec de l'allocation mémoire pour le chemin de la carte.\n");
-        return (NULL);
-    }
-    strcpy(full_path, prefix); //need to add this functions in lbft or in utils 
-    strcat(full_path, map_path); //need to add this function  in utils or libft
-    return (full_path);
-}
 
 int main(int argc, char **argv)
 {
@@ -23,27 +7,31 @@ int main(int argc, char **argv)
     char *map_file_path;
 
     if (argc != 2) {
-        ft_putstr_fd("Usage: ./cub3D <[maps][maps_not_valid]/map.cub>\n", 2);
+        ft_putstr_fd("Usage: ./cub3D <map.cub>\n", 2);
         return (1);
     }
-    map_file_path = call_cub(argv[1]);
-    if (!map_file_path)
-        return (1);
+
+    map_file_path = argv[1];
+
     check_name(map_file_path);
     init_game_struct(&game);
     parse_map_file(map_file_path, &game);
-    free(map_file_path);
     find_player(&game, &game.player.pos_x, &game.player.pos_y, &game.player.start_dir);
     init_player(&game.player, game.player.pos_x, game.player.pos_y, game.player.start_dir);
     load_textures(&game);
     init_window(&game);
+
     game.img.img = mlx_new_image(game.mlx, WIN_WIDTH, WIN_HEIGHT);
-    if (!game.img.img) error_function("Error: failed to create main image buffer");
+    if (!game.img.img)
+        error_function("Error: failed to create main image buffer");
     if (mlx_image_to_window(game.mlx, game.img.img, 0, 0) < 0)
         error_function("Error: failed to attach image buffer to window");
+
     raycasting(&game);
     mlx_loop_hook(game.mlx, game_loop, &game);
     mlx_loop(game.mlx);
+
     free_game(&game);
     return (0);
 }
+
